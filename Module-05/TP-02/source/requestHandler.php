ddebb59b80bb7fb2bbc3d49a8c8dce1a6905927c
yugmerabtene <yug.merabtene@gmail.com>
2024-03-03 01:56:22 +0100

    Delete requestHandler.php

--- Module-05/TP-02/source/requestHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace requestHandler;
-include_once 'functions/model.php';
-include_once 'functions/controller.php';
-/**
- * Contrôleur principal qui gère les différentes actions
- */
-Controller() 
-?>
