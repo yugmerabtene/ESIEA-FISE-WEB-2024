f3171117b66bfc619f47d94d9d73c1bfcc588a37
Yug Merabtène <yug.merabtene@gmail.com>
2024-03-25 10:02:27 +0100

    pushed

---
+++ Module-06/EXO-01/main.js
@@ -0,0 +1,11 @@
+// Acces au DOM en Javascript
+
+// Selectionner un element par son ID
+let elementById = document.getElementById('section3');
+// Selectionner un element par sa Classe
+let elementByClass = document.getElementsByClassName('section2');
+//Selectionner un element par son Tag HTML ( nom de la balise )
+let elementByTag = document.getElementsByTagName('header'); 
+
+// Manipulation du contenu avec JS
+// elementById.textContent = "Nouveau Texte";
