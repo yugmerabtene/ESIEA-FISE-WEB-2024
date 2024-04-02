d3e56948da2cc1994c0dc42c238c3c4638957857
yugmerabtene <yug.merabtene@gmail.com>
2024-04-03 01:35:17 +0200

    update

--- Module-07/TP-01/source/index.php
+++ Module-07/TP-01/source/index.php
@@ -1,21 +1,4 @@
 <?php
-// security.php
-namespace Security;
-
-class CSRFToken {
-    public static function verify($token) {
-        if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
-            throw new \Exception("Erreur CSRF token. Les jetons ne correspondent pas.");
-        }
-    }
-
-    public static function generate() {
-        if (empty($_SESSION['csrf_token'])) {
-            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-        }
-        return $_SESSION['csrf_token'];
-    }
-}
 
 // service.php
 namespace Service;
